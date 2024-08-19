10415d4b409e337b80d7904e6fbd50f6ab2cfabd
MohammedKamalAlsyd <MohammedKamalAlsyd@gmail.com>
2024-08-20 01:26:47 +0300

    Version 0.6 - Solving Frames Problem when running the bot

--- Bot.py
+++ Bot.py
@@ -1,29 +1,31 @@
-from threading import Thread,Lock
+from threading import Thread, Lock
 import pyautogui
+import time
 
-class bot:
+
+class Bot:
     # Threading Properties
-    _stopped = True # To Stop Capture if Stopped
-    _lock = None # Act as Semaphore
+    _stopped = True  # To Stop Capture if Stopped
+    _lock = None  # Act as Semaphore
     # Class Properties
-    _curr_rectangles = [] # store x dim sine it unique across all different three rectangles
-
+    _curr_rectangles = []  # store x dim sine it unique across all different three rectangles
+    _time_treshold = 10
     _original_rectangles = []
     _pixels_treshold = None
 
-    def __init__(self,pixels_treshold = 10):
+    def __init__(self, pixels_treshold=10,time_treshold = 10):
         self._lock = Lock()
         self._pixels_treshold = pixels_treshold
+        self._time_treshold = 10
 
-    def update(self,rec):
+    def update(self, rec):
         self._lock.acquire()
         self._curr_rectangles = rec
         self._lock.release()
         if len(self._original_rectangles) < 3:
             self._update_original(rec)
 
-
-    def _update_original(self,rec):
+    def _update_original(self, rec):
         for r in rec:
             x = r[0]
             # Check if the list is empty or if the new x exceeds the threshold of pixels
@@ -42,18 +44,17 @@ class bot:
                     found = True
                     break
             if not found:
-                missing_indices.append(i + 1)  # Adding 1 to make it 1-based index
+                missing_indices.append(i)
         return missing_indices
-
 
     def start(self):
         self._stopped = False
         t = Thread(target=self._run)
         t.start()
 
-
     def _run(self):
         while not self._stopped:
+            time.sleep(0.01) # increase Frames as this put huge load on cpu
             if self._curr_rectangles is not None and len(self._curr_rectangles) >= 1:
                 # get missing indicies
                 missing = self._find_missing_indices(self._curr_rectangles)
--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from Vision import vision
 from WindowCapture import Screen
 from HSVFilter import HsvFilter, HsvValues
 from time import time
-from Bot import bot
+from Bot import Bot
 from threading import Thread
 import pyautogui
 import cv2 as cv
-
-
-
-
-
-
 
 if __name__ == "__main__":
     # Initialize Classes
@@ -19,44 +13,37 @@ if __name__ == "__main__":
     w, h = scr.getWindowDim()
     vis = vision("Data/Templates/Target.png", w, h)
     hsv_values = HsvValues(0, 0, 100, 179, 132, 255, 0, 0, 50, 0)  # Best Filter After Debugging
-    game_bot = bot(10)
-
+    game_bot = Bot(10)
 
     # Initialize Some Variables
     loop_time = time()
     debug_mode = True
-
 
     # Starting Threads / HSV Filter GUI if needed
     scr.start()
     vis.start(0.7, debug_mode)
     game_bot.start()
 
-
-
-
     while True:
         # Processing the image
         img = scr.getCurrScreen()
         if img is None: continue
-        img = img[int(h//2.2):h, w//4:(3*w)//4]
+        img = img[int(h // 2.2):h, w // 4:(3 * w) // 4]
 
         # Finding Rectangles
-        vis.update(img,hsv_values)
+        vis.update(img, hsv_values)
         rectangles = vis.getRectangles()
         if debug_mode:
             img = vision.drawRectangles(img, rectangles)
             cv.imshow("Debug Screen", img)
 
-
         # Passing Rectangles to the Bot
-        # game_bot.update(rectangles)
-
+        game_bot.update(rectangles)
 
         # Calculate and display the FPS
         if debug_mode:
             fps = 1 / (time() - loop_time)
-            print('FPS: {:.2f}'.format(fps))
+            #print('FPS: {:.2f}'.format(fps))
         loop_time = time()
 
         # Press 'q' with the output window focused to exit
@@ -66,6 +53,5 @@ if __name__ == "__main__":
             game_bot.stop()
             cv.destroyAllWindows()
             break
-
 
     print('Done.')
